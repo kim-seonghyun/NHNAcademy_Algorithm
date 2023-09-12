627adcaaf1a2230b6526ae7eae7843507d1a5b86
김성현 <60726663+kim-seonghyun@users.noreply.github.com>
2023-09-12 11:04:37 +0900

    Create 소인수분해.java

---
+++ 소인수분해.java
@@ -0,0 +1,23 @@
+package org.example;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(soinsu(1234567890, 2, new StringBuilder()).toString());
+    }
+
+    private static double correct = 0;
+
+    public static StringBuilder soinsu(int n, int i, StringBuilder sb) {
+        if (n == 1) {
+            return sb;
+        }
+        if (n % i == 0) {
+            sb.append(i).append(" ");
+            return soinsu(n / i, 2, sb);
+        } else {
+            i += 1;
+            return soinsu(n, i, sb);
+        }
+    }
+}
